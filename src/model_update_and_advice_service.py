87ad062154b98ae2114dbcc2cacff41573316d95
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-05-27 16:11:00 +0800

    feat: Add endpoint to serve local rules file and use shared config loading via util module

--- src/model_update_and_advice_service.py
+++ src/model_update_and_advice_service.py
@@ -5,40 +5,18 @@ from typing import Dict, Any
 import yaml
 import os
 
+import src.util as util
+
+env_variables = util.read_env()
+config = util.load_config(env_variables["cloud_config_path"])
 app = FastAPI()
 
 class DiagnosticInput(BaseModel):
     message: str
 
-def load_config()-> Dict[str, Any]:
-    """
-    Load configuration settings from a YAML file.
-
-    Reads the 'config.yml' file from the current directory
-    and returns its contents as a dictionary.
-
-    Returns:
-        Dict[str, Any]: A dictionary containing configuration keys and values.
-    """    
-    with open("config.yml", "r") as f:
-        config = yaml.safe_load(f)
-    return config
-
-@app.post("/generate_advice")
-def generate_advice(input: DiagnosticInput):
-    msg = input.message.lower()
-    if "thermal" in msg or "90°" in msg:
-        return {
-            "advice": "Your system appears to be running hot. Check for blocked vents and reduce CPU load."
-        }
-    return {
-        "advice": "No specific advice. Please provide a more detailed issue."
-    }
-
 
 @app.post("/load_local_model")
 def load_local_model():
-    config = load_config()
     model_path = config["output"]["model_path"]
 
     if not os.path.exists(model_path):
@@ -47,6 +25,19 @@ def load_local_model():
     return FileResponse(
         path=model_path,
         filename=os.path.basename(model_path),
+        media_type="application/octet-stream"
+    )
+    
+@app.post("/load_local_rules")
+def load_local_rules():
+    rules_path = config["output"]["model_rules"]
+
+    if not os.path.exists(rules_path):
+        return {"error": f"Model not found at: {rules_path}"}
+
+    return FileResponse(
+        path=rules_path,
+        filename=os.path.basename(rules_path),
         media_type="application/octet-stream"
     )
     
