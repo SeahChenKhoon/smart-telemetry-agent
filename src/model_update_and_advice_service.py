967a8e3a7e5c2a3a146e817b5342395ea29b0e4d
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-05-28 16:24:54 +0800

    feat: Integrate LLM-based issue escalation and system diagnostics handling via class-based modules

--- src/model_update_and_advice_service.py
+++ src/model_update_and_advice_service.py
@@ -2,18 +2,29 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from typing import Dict, Any
+import json
+
 import yaml
 import os
 
+from src.cls_env import cls_Env 
+from src.cls_LLM import cls_LLM
+from src.cls_telemetric import cls_SystemDiagnostics
 import src.util as util
 
-env_variables = util.read_env()
-config = util.load_config(env_variables["cloud_config_path"])
+cls_env = cls_Env()
+cls_llm = cls_LLM(cls_env)
+
+config = util.load_config(cls_env.clould_config_path)
+
+
 app = FastAPI()
 
 class DiagnosticInput(BaseModel):
     message: str
 
+class DiagnosticsItem(BaseModel):
+    item: int
 
 @app.post("/load_local_model")
 def load_local_model():
@@ -30,7 +41,7 @@ def load_local_model():
     
 @app.post("/load_local_rules")
 def load_local_rules():
-    rules_path = config["output"]["model_rules"]
+    rules_path = config["output"]["rules_path"]
 
     if not os.path.exists(rules_path):
         return {"error": f"Model not found at: {rules_path}"}
@@ -41,3 +52,27 @@ def load_local_rules():
         media_type="application/octet-stream"
     )
     
+@app.post("/escalate_issue")
+def escalate_issue(diagnostic: DiagnosticInput):
+    rules_path = config["output"]["clould_path"]
+
+    with open(rules_path, "r") as f:
+        rules = json.load(f)
+
+    llm_parameter = {
+        "insert telemetry string here": diagnostic,
+        "insert rules JSON here": rules
+    }
+    output = cls_llm.execute_llm_prompt(config["llm_prompt"], llm_parameter)
+    print(output)
+    return {
+        "response": output
+    }
+
+@app.post("/generate_advice")
+def generate_advice():
+    return {
+        "advice": "No specific advice. Please provide a more detailed issue."
+    }
+
+        
